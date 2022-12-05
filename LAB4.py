6df1a3616a624f83c021d750d2ea386256eb7c19
DrewchBobich <113112410+DrewchBobich@users.noreply.github.com>
2022-12-06 00:42:39 +0300

    Update LAB4.py

--- LAB4.py
+++ LAB4.py
@@ -1,5 +1,5 @@
 #Задани №1
-def chetni(i):
+def chetni(i):  # i - список
     b = []
     for h in range(0, len(i)):
         if h % 2 == 0:
@@ -8,3 +8,26 @@ def chetni(i):
             continue
     return b
 
+#Задание №2
+def bolshe(i):   # i - список
+    b = []
+    for h in range(len(i)-1):
+        if i[h + 1] > i[h]:
+            b.append(i[h + 1])
+        else:
+            continue
+    return b
+
+#Задание №3
+def prikol(i):   # i - список
+    bolsh = max(i)
+    mensh = min(i)
+    b = i.index(min(i))
+    a = i.index(max(i))
+    i[b] = bolsh
+    i[a] = mensh
+    return i  
+
+#Задание №4
+def chtoto(i, b):   # i - словарь, b - ключ
+  print(i[b])
