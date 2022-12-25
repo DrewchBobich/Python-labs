10cd0e420ee3f54fa1817f70795fdcad91c83e3e
DrewchBobich <franck_g@list.ru>
2022-12-25 13:24:51 +0300

    Добавил

--- LAB4.py/figures.py/__init__.py
+++ LAB4.py/figures.py/__init__.py
@@ -1,6 +1,6 @@
-from .circle import circle_area, circle_perimeter
-from .triangle import triangle_area, triangle_perimeter
-from .square import square_area, square_perimeter
+from . import circle_area, circle_perimeter
+from . import triangle_area, triangle_perimeter
+from . import square_area, square_perimeter
 
 __all__ = (
     'circle_area',
